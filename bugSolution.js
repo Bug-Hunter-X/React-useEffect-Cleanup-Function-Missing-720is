f37f6a4c91e09c0f20a4494e4fe9c4741ddeeaa5
BugHunter-X <askzenai@gmail.com>
2025-01-10 23:38:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,18 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    // Correct: Cleanup function clears the interval
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>Count: {count}</div>
+  );
+}
+```
